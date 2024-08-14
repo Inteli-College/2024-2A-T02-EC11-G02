b7b00c845b4b4486f673054ecce8f0ed1cd024ac
JvLetsBora <jv.oliveira.designer@gmail.com>
2024-08-14 09:47:41 -0300

    feat: add frontend configurations

---
+++ src/frontend/src/app/page.js
@@ -0,0 +1,10 @@
+
+export default function App() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
+        <h1 className="text-2xl font-bold">App no ar &#128522;</h1>
+      </div>
+    </main>
+  );
+}
