5cbfa840e952f8c927c788b50dbc723b6a5f5e63
JvLetsBora <jv.oliveira.designer@gmail.com>
2024-08-14 09:51:25 -0300

    feat: add frontend routers

--- src/frontend/src/pages/home/index.js
+++ src/frontend/src/pages/home/index.js
@@ -1,8 +1,9 @@
+import "../../app/globals.css";
 import React from "react";
 
 export default function Home() {
     return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex bg-stone-800 min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
           <h1 className="text-2xl font-bold">Home ? ;/</h1>
         </div>
