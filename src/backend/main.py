215a6e889fe0b43c8d5e002e57d5f9fac27a10bd
Luca Giberti <luca.giberti@sou.inteli.edu.br>
2024-08-23 07:04:03 -0300

    fixed spelling error and added router

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from .routers import pullArrayBytes, modelVersion, S3Router
+from .routers import pullArrayBytes, modelVersion, S3Router, imageHash
 
 app = FastAPI()
 
 app.include_router(pullArrayBytes.router)
 app.include_router(modelVersion.router)
 app.include_router(S3Router.router)
+app.include_router(imageHash.router)
