fe9356b3fea415484eccd6d1992bd1e28275250c
JvLetsBora <jv.oliveira.designer@gmail.com>
2024-09-02 08:26:46 -0300

    ...

--- tools/src/applay_filter.py
+++ tools/src/applay_filter.py
@@ -8,7 +8,7 @@ tools = ImageFilters()
 def plot_images(image, title, position, cmap=None):
     plt.subplot(2, 3, position)
     plt.title(title)
-    plt.imshow(image, cmap=cmap)
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), cmap=cmap)
 
 def remuve_background_and_plot(image_path):
     # Carregar a imagem
@@ -17,20 +17,9 @@ def remuve_background_and_plot(image_path):
     # Separar os canais R, G, B
     B, G, R = cv2.split(image)
 
-    # Aplicar as transformações no canal R
-    R = tools.apply_brightness_contrast(R, 20, 1.5)
-    R = tools.apply_curves(R, np.array([[0, 0], [105, 92], [146, 247], [255, 255]]))
-    R = tools.apply_kernal_bluer(R, 5)
-    R = tools.level_image_numpy(R, 200, 255, 9.9)
-    
-    #save_image(R, 'dataset/978039_sat_41_R.jpg')
-    # Normalizar a máscara para ter valores entre 0 e 255
-    _mask = cv2.normalize(R, None, 0, 255, cv2.NORM_MINMAX)
-    #_mask = cv2.merge([_mask, _mask, _mask])
-    _mask_inverted = cv2.bitwise_not(_mask)
-
     # Aplicar a máscara à imagem original
-    masked_image = cv2.bitwise_and(image, _mask_inverted)
+    mask = get_mask(image_path)
+    masked_image = cv2.bitwise_and(image, mask)
 
 
 
@@ -43,7 +32,8 @@ def remuve_background_and_plot(image_path):
     plot_images(G, "Canal G", 2,"gray")
     plot_images(B, "Canal B", 3,"gray")
     plot_images(image, "Original", 4,)
-    plot_images(masked_image, "Final", 5)
+    plot_images(mask, "mask", 5)
+    plot_images(masked_image, "Final", 6)
     
     # Mostrar as imagens
     plt.show()
@@ -73,15 +63,24 @@ def get_mask(image_path):
 
 def segment_and_plot(image_path):
     image = cv2.imread(image_path)
-    image = tools.apply_color(image,[ 0, 0, 0])
+    image = tools.apply_color(image,[ 0, 0, 10 ])
     masked_image = cv2.bitwise_and(image, get_mask(image_path))
     plt.figure(figsize=(10, 10))
-    plt.imshow(masked_image)
+    plt.imshow(cv2.cvtColor(masked_image, cv2.COLOR_BGR2RGB))
     plt.show()
 
-# Exemplo de uso
-image_path = 'dataset/06.png'  
-#remuve_background_and_plot(image_path)
-segment_and_plot(image_path)
+def remuve_background(image_path):
+    image = cv2.imread(image_path)
+    target_image = cv2.bitwise_and(image, get_mask(image_path))
+    return target_image
 
-#save_image(get_mask(image_path), 'processed_images/978039_sat_41_mask.jpg')
+
+
+
+# Exemplo de uso
+image_path = f'dataset/maps_02.png'  
+
+remuve_background_and_plot(image_path)
+#segment_and_plot(image_path)
+#plot_images(remuve_background(image_path), "Final", 1)
+plt.show()
