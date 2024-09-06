bcca98bd99e8e8fd9891575d6b8f45a47d7443d8
Luca Giberti <luca.giberti@sou.inteli.edu.br>
2024-09-06 10:11:53 -0300

    route to save image working

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from backend.routers import S3Router, modelVersion, saveImage, pullArrayBytes
+from routers import modelVersion, saveImage, pullArrayBytes
 from dotenv import load_dotenv
 from firebase_admin import credentials
 from firebase_admin import storage
@@ -7,28 +7,42 @@ import firebase_admin
 import os
 
 load_dotenv()
+app = FastAPI()
 
 firebase_storage_bucket = os.getenv('FIREBASE_STORAGE_BUCKET')
 firebase_key_path = os.getenv('FIREBASE_KEY_PATH')
 
-current_directory = os.path.dirname(os.path.abspath(__file__))
-key_path = os.path.join(current_directory, firebase_key_path)
-cred = credentials.Certificate(key_path)
+cred = credentials.Certificate(firebase_key_path)
 firebase_admin.initialize_app(cred, {
     'storageBucket': firebase_storage_bucket
 })
 
-bucket = storage.bucket()
+# Função para fazer upload da imagem para o Firebase Storage
+def upload_image(file_path, destination_blob_name):
+    # Referência ao bucket de armazenamento
+    bucket = storage.bucket()
+    
+    # Referência ao local no storage onde a imagem será salva
+    blob = bucket.blob(destination_blob_name)
+    
+    # Fazendo o upload da imagem
+    blob.upload_from_filename(file_path)
+    
+    # Opcional: Tornar o arquivo público (caso precise de acesso público)
+    blob.make_public()
+    
+    print(f"Imagem enviada com sucesso! URL pública: {blob.public_url}")
 
-# 'bucket' is an object defined in the google-cloud-storage Python library.
-# See https://googlecloudplatform.github.io/google-cloud-python/latest/storage/buckets.html
-# for more details.
+# Envie sua imagem
+upload_image("bode.webp", "sucesso/bode.webp")
 
-app = FastAPI()
+#app.include_router(S3Router.router)
+#app.include_router(saveImage.router)
+#app.include_router(modelVersion.router)
+#app.include_router(pullArrayBytes.router)
 
-app.include_router(S3Router.router)
-app.include_router(saveImage.router)
-app.include_router(modelVersion.router)
-app.include_router(pullArrayBytes.router)
+#saveImage.upload_image("")
 
-saveImage.upload_image(bucket)
+if "__main__" == __name__:
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
