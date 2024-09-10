d5219f719d1f39fe64bbf2aab787ae1c89e8eec5
Luca Giberti <luca.giberti@sou.inteli.edu.br>
2024-09-10 09:25:20 -0300

    modularized upload image route works, pullarraybytes file deleted and savaImage route works with zip file with multiple images inside

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
-from routers import modelVersion, saveImage, pullArrayBytes
+from routers import modelVersion, saveImage
 from dotenv import load_dotenv
 from firebase_admin import credentials
-from firebase_admin import storage
 import firebase_admin
 import os
 
@@ -17,31 +16,13 @@ firebase_admin.initialize_app(cred, {
     'storageBucket': firebase_storage_bucket
 })
 
-# Função para fazer upload da imagem para o Firebase Storage
-def upload_image(file_path, destination_blob_name):
-    # Referência ao bucket de armazenamento
-    bucket = storage.bucket()
-    
-    # Referência ao local no storage onde a imagem será salva
-    blob = bucket.blob(destination_blob_name)
-    
-    # Fazendo o upload da imagem
-    blob.upload_from_filename(file_path)
-    
-    # Opcional: Tornar o arquivo público (caso precise de acesso público)
-    blob.make_public()
-    
-    print(f"Imagem enviada com sucesso! URL pública: {blob.public_url}")
+# Include the saveImage router
+app.include_router(saveImage.router)
 
-# Envie sua imagem
-upload_image("bode.webp", "sucesso/bode.webp")
-
-#app.include_router(S3Router.router)
-#app.include_router(saveImage.router)
-#app.include_router(modelVersion.router)
-#app.include_router(pullArrayBytes.router)
-
-#saveImage.upload_image("")
+# Other routers
+# app.include_router(S3Router.router)
+# app.include_router(modelVersion.router)
+# app.include_router(pullArrayBytes.router)
 
 if "__main__" == __name__:
     import uvicorn
