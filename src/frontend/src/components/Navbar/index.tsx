cbdee740c75dab8612003e324f5444b16f90cf87
jacksonwsaguiar <jackson.aguiar@sou.inteli.edu.br>
2024-09-12 09:13:06 -0300

    frontend finished

--- src/frontend/src/components/Navbar/index.tsx
+++ src/frontend/src/components/Navbar/index.tsx
@@ -1,79 +1,61 @@
 import { Link } from "react-router-dom";
-import { Abundance } from "../../assets";
+import { Abundance, Ares } from "../../assets";
+import { Outlet } from "react-router-dom";
 
 const NavbarComponent = () => {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link
-          to="/home"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <img
-            src={Abundance}
-            className="h-8"
-            alt="Abundance logo"
-          />
-    
-        </Link>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                aria-current="page"
-                to="/reports"
-              >
-                Relatórios
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/upload"
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                aria-current="page"
-              >
-                Upload
-              </Link>
-            </li>
+    <div className="h-screen">
+      <nav className="bg-[#2C2F53] border-gray-200 dark:bg-gray-900 h-20">
+        <div className=" flex flex-wrap items-center justify-between mx-auto p-4">
+          <div className="flex">
+            <div
+              className="flex items-center space-x-3 rtl:space-x-reverse"
+            >
+              <img src={Abundance} className="h-8" alt="Abundance logo" />
+            </div>
+            <div
+              className="hidden w-full md:block md:w-auto"
+              id="navbar-default"
+            >
+              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
+                <li>
+                  <Link
+                    className="block py-2 px-3 text-white bg-transparent"
+                    aria-current="page"
+                    to="/app/reports"
+                  >
+                    Relatórios
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/app/upload"
+                    className="block py-2 px-3 text-white bg-transparent"
+                    aria-current="page"
+                  >
+                    Upload
+                  </Link>
+                </li>
 
-            <li>
-              <Link
-                aria-current="page"
-                to="/history"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Histórico
-              </Link>
-            </li>
-          </ul>
+                <li>
+                  <Link
+                    aria-current="page"
+                    to="/app/history"
+                    className="block py-2 px-3 text-white bg-transparent"
+                  >
+                    Histórico
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <img src={Ares} className="h-8" alt="Abundance logo" />
         </div>
+      </nav>
+      <div className="h-full">
+        <Outlet />
       </div>
-    </nav>
+    </div>
   );
 };
 
--- src/frontend/src/pages/app/upload/index.tsx
+++ src/frontend/src/pages/app/upload/index.tsx
@@ -1,9 +1,46 @@
+import Select from "react-select";
+
+const options = [
+  { value: "brasil-sudeste", label: "Brasil - Sudeste" },
+  { value: "brasil-sul", label: "Brasil - Sul" },
+  { value: "brasil-nordeste", label: "Brasil - Nordeste" },
+  { value: "brasil-norte", label: "Brasil - Norte" },
+  { value: "brasil-centro-oeste", label: "Brasil - Centro-Oeste" },
+];
+
 export default function UploadPage() {
-    return (
-      <div>
-        <h1>upload Page</h1>
-        <p>Welcome to the home page!</p>
+  return (
+    <div>
+      <div className="flex justify-evenly p-14">
+        <div className="w-1/3">
+          <h1 className="font-bold text-[#575EA6]">Teste de modelo</h1>
+          <p>
+            O ESG é uma das tendências mais relevantes de mercado dos últimos
+            tempos. Convidamos você a fazer parte do nosso universo de
+            abundância.
+          </p>
+        </div>
+        <div className="flex flex-col w-1/2 ">
+          <div className="flex">
+            <div>
+              <label className="font-bold">Escolha a região</label>
+              <Select options={options} className="mb-6" />
+              <label htmlFor="" className="font-bold">
+                Upload files
+              </label>
+              <p>
+                Somente arquivos .jpg e .png. Tamanho máximo de arquivo de 5 MB.
+              </p>
+              <div className="border-dashed flex justify-center items-center border-blue-500 border rounded-lg w-full h-20">
+                Carregue uma imagem
+              </div>
+              {/* <input type="file" /> */}
+            </div>
+            <div className="w-full h-64 bg-black ml-2 rounded-lg"></div>
+          </div>
+          <button type="button" className=" w-28 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Enviar</button>
+        </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
