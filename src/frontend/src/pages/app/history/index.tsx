b9870154d6b4cea347b8d282690d1adc7bf89557
jacksonwsaguiar <jackson.aguiar@sou.inteli.edu.br>
2024-09-13 08:17:44 -0300

    changed bg dark

--- src/frontend/src/pages/app/history/index.tsx
+++ src/frontend/src/pages/app/history/index.tsx
@@ -6,8 +6,8 @@ export default function HistoryPage() {
       <h1 className="mb-8 font-bold text-center text-[#575EA6] text-2xl">Dados relacionados as florestas contabilizadas</h1>
 
       <div className="relative overflow-x-auto">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
               <th scope="col" className="px-6 py-3"></th>
               <th scope="col" className="px-6 py-3">
@@ -29,10 +29,10 @@ export default function HistoryPage() {
           </thead>
           <tbody>
             {[0, 1, 2, 3].map((item) => (
-              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <tr className="bg-white border-b">
                 <th
                   scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-36"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
                 >
                   <img src={Map} alt="" />
                 </th>
