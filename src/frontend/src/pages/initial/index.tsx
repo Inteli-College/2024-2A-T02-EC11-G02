51d34a0c94edbedd5a8a0858eb5390bd4a471898
jacksonwsaguiar <jackson.aguiar@sou.inteli.edu.br>
2024-09-13 08:02:18 -0300

    changed router

--- src/frontend/src/pages/initial/index.tsx
+++ src/frontend/src/pages/initial/index.tsx
@@ -17,7 +17,7 @@ export default function InitialPage() {
         <button
           type="button"
           onClick={() => {
-            window.location.href = "/app";
+            window.location.href = "/app/reports";
           }}
           className="text-white mt-5 hover:text-black hover:bg-white border border-whit focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
         >
